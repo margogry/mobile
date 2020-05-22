8d124d18b49e614ad41f86f2a7d8a198a1d95fe2
margogry <47557010+margogry@users.noreply.github.com>
2020-05-23 00:15:25 +0300

    Add files

---
+++ lab123/lab1.py
@@ -0,0 +1,54 @@
+info = []
+
+class dataCDR (object):
+    def __init__(self, date = 0, time = 0, numFrom = 0, numTo = 0, duration = 0, sms = 0):
+        self.time = time
+        self.date = date
+        self.numTo = numTo
+        self.numFrom = numFrom
+        self.duration = duration
+        self.sms = sms
+
+
+def inputData():
+    F = open("data", "r")
+    f = F.read().replace("\n", " ").split(" ")
+    #print(f)
+
+    for i in range ((int)(len(f)/6) - 1):
+        info.append(dataCDR())
+        info[i].data = f[i*6]
+        info[i].time = f[i*6 + 1]
+        info[i].numFrom = f[i*6 + 2]
+        info[i].numTo = f[i*6 + 3]
+        info[i].duration = f[i*6 + 4]
+        info[i].sms = f[i*6 + 5]
+
+
+def moreZero (a):
+    if a < 0:
+        return 0
+    return a
+
+def tarific1():
+    inputData()
+    who = '915783624' #input('ведите номер абонента - ')
+    outRing = 2 #input('Сколько для него стоят исходящие звонки? - ')
+    inRing = 0 #input('Сколько стоят входящие звонки? - ')
+    freeSMS = 10 #input('Сколько бесплатных СМС? - ')
+    costSMS = 1 #input('Сколько стоят последующие СМС? - ')
+    res = 0
+    for i in range (len(info)):
+        if info[i].numTo == who:
+            res += (float)(info[i].duration) * (float)(inRing)
+            #print(res, ' info[i].duration = ', info[i].duration, ' inRing = ', inRing)
+        if info[i].numFrom == who:
+            res += (float)(info[i].duration) * (float)(outRing)
+            #print(res, ' info[i].duration = ', info[i].duration, ' outRing = ', outRing)
+            res += (moreZero((int)(info[i].sms) - (int)(freeSMS))) * (int)(costSMS)
+            #print(res, ' info[i].sms = ', info[i].sms, ' costSMS = ', costSMS)
+    return res
+
+
+#print('Тарификация услуг - ', tarific1())
+
--- lab123/lab3.py
+++ lab123/lab3.py
@@ -1,1 +1,40 @@
+from docxtpl import DocxTemplate
+import comtypes.client
 
+from lab1 import *
+from lab2 import *
+
+resLab1 = tarific1()
+resLab2 = tarific2()
+
+doc = DocxTemplate("lab3_tmpl.docx")
+context = { 'bank' : 'ОАО Твой Банк',
+            'bik' : '123456',
+            'kpp':'123456',
+            'reciever':'ОАО Интернет',
+            'first': '12345678910',
+            'second': '12345678910',
+            'inn': '123456789',
+            'address1' : 'г. Санкт-Петербург, ул. Леносовета, 23',
+            'index' : '123456',
+            'address2' : 'г. Санкт-Петербург, ул. Вавиловых, 12к2',
+            'osnovanie':'Договор №6 от 01.03.2045',
+            'name1':'Телефония',
+            'name2':'Интернет',
+            'sum1': resLab1,'sum2': resLab2,
+            'overall': resLab1 + resLab2,
+            'director':'Брунова.А.М.',
+            'accountant':'Брунова.А.М.',
+            'n':'2','d':'1 июня','y':'20', 'buy':'ООО Что-то Интересное', 'rus': 'rub'}
+doc.render(context)
+doc.save("lab3.docx")
+wdFormatPDF = 17
+
+in_file = r"C:\\Users\\Анастасия\\PycharmProjects\\mobile\\lab3\\lab3.docx"
+out_file = r"C:\\Users\\Анастасия\\PycharmProjects\\mobile\\lab3\\lab3.pdf"
+
+word = comtypes.client.CreateObject('Word.Application')
+doc = word.Documents.Open(in_file)
+doc.SaveAs(out_file, FileFormat=wdFormatPDF)
+doc.Close()
+word.Quit()
